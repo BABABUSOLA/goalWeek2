cca37558a926d3d78b2d16982d1de3d5c128e9c3
bababusola <queenbusola1@gmail.com>
2016-06-02 23:43:35 +0100

    first commit

---
+++ loops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n =  "); 
+          int n = int.Parse(Console.ReadLine());
+          for (int row = 1; row <= n; row++)
+          {
+              for (int column = 1; column <= row; column++)
+              {
+                  Console.Write(column);
+              }
+              Console.WriteLine();
+          }
+        }
+    }
+}
---
+++ operatorsInInnerMostLoop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operatorsInInnerMostLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            /*Console.Write("Please enter the positive integer, n = ");
+            string numberAsString = Console.ReadLine();
+            int n = Convert.ToInt32(Console.ReadLine());
+            // calculate n! = 1*2*....*n
+            int factorial = n;
+           do
+            {
+                factorial *= n;
+                n--;
+            }
+            while( n > 0) ;
+            Console.WriteLine("n! = " + factorial);
+             */
+            //A simple loop to calculate n!
+            
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            decimal factorial = 1;
+
+            for (int i = 1; i <= n; i++)
+			{
+			 factorial *= i;
+			}
+            Console.WriteLine("factorial = {0}", factorial);
+        }
+    }
+}
